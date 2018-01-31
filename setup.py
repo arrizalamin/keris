48d3b98172e7bd232de285a08fa0542ebdf1f07e
Arrizal Amin <arrizalamin@gmail.com>
2018-01-31 12:41:31 +0700

    modify setup.py to publish to pypi

--- setup.py
+++ setup.py
@@ -1,27 +1,14 @@
 from setuptools import setup
-# from distutils.extension import Extension
-# from Cython.Build import cythonize
-# import numpy
-
-# extensions = [
-#     Extension('im2col_cython', ['im2col_cython.pyx'],
-#               include_dirs=[numpy.get_include()]
-#               ),
-# ]
-
-# setup(
-#     ext_modules=cythonize(extensions),
-# )
-
 
 setup(name='keris',
-      version='1.0',
+      version='0.1',
       description='Keras-like deep learning framework built with numpy',
       url='https://github.com/arrizalamin/keris',
+      download_url='https://github.com/arrizalamin/keris/archive/0.1.tar.gz',
       author='Arrizal Amin',
       author_email='arrizalamin@gmail.com',
       license='MIT',
       packages=['keris'],
-      install_requires=['numpy>=1.9.1',
-                        'tqdm'],
-      zip_safe=False)
+      keywords=['deep learning', 'keris'],
+      install_requires=['numpy>=1.13.3',
+                        'tqdm>=4.19.4'])
