55074669474428f0a86a567cfae226aed16875f4
Arrizal Amin <arrizalamin@gmail.com>
2018-02-16 04:40:14 +0700

    fix backward computation of softmax

--- keris/activations/__init__.py
+++ keris/activations/__init__.py
@@ -1,1 +1,2 @@
 from keris.activations.rectifier import ReLU, LeakyReLU, ELU
+from keris.activations.probability import Softmax
--- keris/activations/probability.py
+++ keris/activations/probability.py
@@ -1,5 +1,5 @@
 import numpy as np
-from layers.layer import Layer
+from keris.layers.layer import Layer
 
 
 class Softmax(Layer):
@@ -16,9 +16,21 @@ class Softmax(Layer):
     def forward(self, x, mode):
         out = np.exp(x - np.max(x, axis=self.axis, keepdims=True))
         out /= np.sum(out, axis=self.axis, keepdims=True)
+        self.x, self.out = x, out
 
         return out
 
     def backward(self, dout, mode):
-        dx = np.exp(dout)
+        """
+        https://stackoverflow.com/a/40576872
+        """
+        x, out = self.x, self.out
+        dx = np.zeros_like(dout)
+
+        for i in range(len(out)):
+            for j in range(len(x)):
+                if i == j:
+                    dx[i, j] = out[i] * (1 - x[i])
+                else:
+                    dx[i, j] = -out[i] * x[j]
         return dx, None
