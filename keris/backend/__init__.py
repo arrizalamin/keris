bb47c2795af763a97a7a7676b1b6f715f3a41cdf
Arrizal Amin <arrizalamin@gmail.com>
2018-02-20 07:37:03 +0700

    add cupy backend

--- keris/backend/__init__.py
+++ keris/backend/__init__.py
@@ -1,11 +1,18 @@
 import sys
+import os
 from .ndarray import *
 
 _BACKEND = 'numpy'
 
+if 'KERIS_BACKEND' in os.environ:
+    _BACKEND = os.environ['KERIS_BACKEND']
+
 if _BACKEND == 'numpy':
     sys.stderr.write('Using numpy backend\n')
     from numpy import *
+elif _BACKEND == 'cupy':
+    sys.stderr.write('Using cupy backend\n')
+    from cupy import *
 
 
 def backend():
